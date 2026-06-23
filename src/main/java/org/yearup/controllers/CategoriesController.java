097f7ab8b91b28857dc5104ee042dacbf79e0cce
Arsema-y <meleymm88@gmail.com>
2026-06-23 10:55:30 -0500

    Initial commit; Completes debugging Categories (phase-1)

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,92 @@
+package org.yearup.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+import org.yearup.service.CategoryService;
+import org.yearup.service.ProductService;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("categories") // http://localhost:8080/categories
+@CrossOrigin
+public class CategoriesController
+{
+    private final CategoryService categoryService;
+    private final ProductService productService;
+
+    public CategoriesController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Category> getAll()
+    {
+        // find and return all categories
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable int id)
+    {
+        // get the category by id
+        Category category = categoryService.getById(id);
+
+        if(category == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return category;
+    }
+
+
+    @GetMapping("{categoryId}/products") // https://localhost:8080/categories/1/products
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return productService.listByCategoryId(categoryId);
+    }
+
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Category> addCategory(@RequestBody Category category)
+    {
+        // insert the category and return it with status 201 Created
+        Category saved = categoryService.create(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Category updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        // update the category by id and return the updated category (200 OK)
+        if (categoryService.getById(id) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return categoryService.update(id, category);
+    }
+
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id)
+    {
+        // delete the category by id and return status 204 No Content
+        if (categoryService.getById(id) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/org/yearup/service/CategoryService.java
@@ -0,0 +1,56 @@
+package org.yearup.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.models.Category;
+import org.yearup.repository.CategoryRepository;
+
+import java.util.List;
+
+@Service
+public class CategoryService
+{
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository)
+    {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories()
+    {
+        // get all categories
+        return categoryRepository.findAll();
+    }
+
+    public Category getById(int categoryId)
+    {
+        // get category by id
+        return categoryRepository.findById(categoryId)
+                .orElseThrow (()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not Found"));
+    }
+
+    public Category create(Category category)
+    {
+        // create a new category
+        return categoryRepository.save(category);
+    }
+
+    public Category update(int categoryId, Category category)
+    {
+        // update category and return the updated category
+
+        Category existing = categoryRepository.findById(categoryId).orElseThrow();
+        existing.setName(category.getName());
+        existing.setDescription(category.getDescription());
+
+        return categoryRepository.save(existing);
+    }
+
+    public void delete(int categoryId)
+    {
+        // delete category
+        categoryRepository.deleteById(categoryId);
+    }
+}
